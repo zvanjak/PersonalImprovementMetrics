a6b7402f851f3bd78547d5d775b19d0a7040df3c
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-01-10 19:36:46 +0100

    radno

--- PIM.Domain/MetricMeasurement.cs
+++ PIM.Domain/MetricMeasurement.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace PIM.Domain
 {
+    // ovo su u biti PRVAE METRIKE - ono što se mjeri - koliko si večerao, kad si išao spavati, koliko si težak, jesi li odradio trening u teretani
+    // a onda se nad tim podacima mogu definirati Performance metrike
     public class MetricMeasurement
     {
         string _noteText;
